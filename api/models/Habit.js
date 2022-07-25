2618d65fa97ed1433308d3bd993becaca1175cc0
Stefan <stefan.d.yordanov@gmail.com>
2022-07-25 17:55:45 +0100

    add Habit model

--- api/models/Habit.js
+++ api/models/Habit.js
@@ -1,14 +1,56 @@
 const db = require("../dbConfig/mongo/dbConfig");
 
-const habitSchema = new db.Schema({
-    name: String,
-    userID: Number,
-    frequency: String,
-    date: {
-        now: new Date(),
-    }
+const trackingSchema = new db.Schema({
+    frequency: {
+        daily: {
+            day: () => {
+                const now = new Date();
+                const options = {
+                    weekday: "long",
+                };
+
+                return new Intl.DateTimeFormat("en-GB", options).format(now);
+            },
+            status: String,
+        },
+        weekly: [
+            {
+                days: [
+                    { mon: Boolean },
+                    { tue: Boolean },
+                    { wed: Boolean },
+                    { thu: Boolean },
+                    { fri: Boolean },
+                    { sat: Boolean },
+                    { sun: Boolean },
+                ],
+                status: String,
+            },
+        ],
+    },
 });
+
+const habitSchema = new db.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    completed: Boolean,
+    tracking: trackingSchema,
+    userID: Number,
+});
+
+habitSchema.statics.findByName = function (name) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const habit = this.findOne({ name: name });
+            resolve(habit);
+        } catch (error) {
+            reject("Habit not found");
+        }
+    });
+};
 
 const Habit = db.model("Habit", habitSchema);
 
-Habit.
+module.exports = Habit;
