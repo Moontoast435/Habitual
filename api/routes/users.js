dc06d174919b38f6add0af39e9161a7473d0e8c2
Peesh98 <timothy.t.le98@gmail.com>
2022-07-25 19:17:07 +0100

    Updated _scripts and did some debugging on running the api

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyToken } = require('../middleware/auth')
+const verifyToken = require('../middleware/auth')
 const usersController = require('../controllers/users');
 
 router.get('/:username', verifyToken, usersController.showUser)
