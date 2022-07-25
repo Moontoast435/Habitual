6b69bd05a5679380ac20c8b4ce6605c68938d2c5
Joshua Bankole-Williams <joshua.bankole-williams@getfutureproof.co.uk>
2022-07-25 17:30:12 +0100

    configured the auth controller, added Auth functionality to both register and login functions

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
+
+const bcrypt = require('bcryptjs');
+
+const jwt = require("jsonwebtoken");
+
 const User = require('../models/user'); // will need to match file name
+
+
 
 async function register (req, res) {   // creating/registering a new user - will need to add Auth functionality
     try {
-        const user = await User.create(req.body.username, req.body.email, req.body.password);   //need to change these fields to match stefans models & the params he sets for his create method
-        res.status(201).json(user);
+        const salt = await bcrypt.genSalt();
+        const hashedPw = await bcrypt.hash(req.body.password, salt)
+        const user = await User.create({...req.body, password: hashedPw});   //need to change these fields to match stefans models & the params he sets for his create method
+        res.status(201).json({msg: `This user: ${user.username} was successfully created`});
     } catch (err) {
         res.status(422).json({err})
     }
@@ -11,9 +21,29 @@ async function register (req, res) {   // creating/registering a new user - will
 
 async function login (req, res) {   //logging in - will need to add Auth
     try {
-        res.status(201)
+        const user = await User.findByUsername(req.body.username)
+        if(!user) {
+            throw new Error('No user with this username')
+        }
+        const authed = bcrypt.compare(req.body.password, user.hashedPassword)
+        if(!!authed) {
+            const payload = { username: user.username }
+            const sendToken = (err, token) => {
+                if(err) {
+                    throw new Error('Error when generating token')
+                }
+                res.status(200).json({
+                    success: true,
+                    token: "Bearer" + token,
+                });
+            }
+            jwt.sign(payload, process.env.SECRET, {expiresIn: 1200}, sendToken);
+        } else {
+            throw new Error('User could not be authenticated')
+        }
     } catch (err) {
-        res.status(404)
+        console.log(err);
+        res.status(401).json({err});
     }
 }
 
