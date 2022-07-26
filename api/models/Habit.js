0dc17555ce105a862667642413263881dc5cb425
Stefan <stefan.d.yordanov@gmail.com>
2022-07-26 09:36:30 +0100

    Add more functionality to the Habit model and update the schema

--- api/models/Habit.js
+++ api/models/Habit.js
@@ -1,33 +1,66 @@
 const db = require("../dbConfig/mongo/dbConfig");
+const User = require("./User");
 
 const trackingSchema = new db.Schema({
-    frequency: {
-        daily: {
-            day: () => {
-                const now = new Date();
-                const options = {
-                    weekday: "long",
-                };
+    dates: [
+        {
+            day: {
+                type: String,
+                default: () => {
+                    const now = new Date();
+                    const options = {
+                        weekday: "long",
+                    };
 
-                return new Intl.DateTimeFormat("en-GB", options).format(now);
+                    return new Intl.DateTimeFormat("en-GB", options).format(now);
+                },
             },
-            status: String,
+            date: {
+                type: String,
+                default: () => {
+                    const now = new Date();
+                    const options = {
+                        day: "numeric",
+                        month: "numeric",
+                        year: "numeric",
+                    };
+
+                    return new Intl.DateTimeFormat("en-GB", options).format(now);
+                },
+            },
+            complete: Boolean,
         },
-        weekly: [
-            {
-                days: [
-                    { mon: Boolean },
-                    { tue: Boolean },
-                    { wed: Boolean },
-                    { thu: Boolean },
-                    { fri: Boolean },
-                    { sat: Boolean },
-                    { sun: Boolean },
-                ],
-                status: String,
-            },
-        ],
-    },
+    ],
+    // frequency: {
+    //     daily: {
+    //         day: {
+    //             type: String,
+    //             default: () => {
+    //                 const now = new Date();
+    //                 const options = {
+    //                     weekday: "long",
+    //                 };
+
+    //                 return new Intl.DateTimeFormat("en-GB", options).format(now);
+    //             },
+    //         },
+    //         status: String,
+    //     },
+    //     weekly: [
+    //         {
+    //             days: [
+    //                 { mon: Boolean },
+    //                 { tue: Boolean },
+    //                 { wed: Boolean },
+    //                 { thu: Boolean },
+    //                 { fri: Boolean },
+    //                 { sat: Boolean },
+    //                 { sun: Boolean },
+    //             ],
+    //             status: String,
+    //         },
+    //     ],
+    // },
 });
 
 const habitSchema = new db.Schema({
@@ -35,15 +68,41 @@ const habitSchema = new db.Schema({
         type: String,
         required: true,
     },
-    completed: Boolean,
     tracking: trackingSchema,
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
     userID: Number,
 });
+
+habitSchema.statics.getUsersHabits = function (username) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findByUsername(username);
+            const habits = await this.find({ userID: user.id }).toArray();
+            resolve(habits);
+        } catch (error) {
+            reject("No habits found");
+        }
+    });
+};
+habitSchema.statics.getUsersHabits = function (username) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findByUsername(username);
+            const habits = await this.find({ userID: user.id }).toArray();
+            resolve(habits);
+        } catch (error) {
+            reject("No habits found");
+        }
+    });
+};
 
 habitSchema.statics.findByName = function (name) {
     return new Promise(async (resolve, reject) => {
         try {
-            const habit = this.findOne({ name: name });
+            const habit = await this.findOne({ name: name });
             resolve(habit);
         } catch (error) {
             reject("Habit not found");
