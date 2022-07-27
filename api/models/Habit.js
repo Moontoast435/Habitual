aa625877ced864a86ecd821056889c4b5748f0bd
Moontoast435 <seanmalone1993@gmail.com>
2022-07-27 17:41:18 +0100

    pre-merge commit

--- api/models/Habit.js
+++ api/models/Habit.js
@@ -3,93 +3,96 @@ const connection = require("../dbConfig/mongo/connection");
 const User = require("./User");
 
 const habitSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+  dates: [
+    {
+      day: {
         type: String,
-        required: true,
-    },
-    dates: [
-        {
-            day: {
-                type: String,
-                default: () => {
-                    const now = new Date();
-                    const options = {
-                        weekday: "long",
-                    };
+        default: () => {
+          const now = new Date();
+          const options = {
+            weekday: "long",
+          };
 
-                    return new Intl.DateTimeFormat("en-GB", options).format(now);
-                },
-            },
-            date: {
-                type: String,
-                default: () => {
-                    const now = new Date();
-                    const options = {
-                        day: "numeric",
-                        month: "numeric",
-                        year: "numeric",
-                    };
-
-                    return new Intl.DateTimeFormat("en-GB", options).format(now);
-                },
-            },
-            complete: Boolean,
+          return new Intl.DateTimeFormat("en-GB", options).format(now);
         },
-    ],
-    frequency: {
-        daily: Boolean,
-        weekly: Boolean,
-    },
-    createdAt: {
+      },
+      date: {
         type: String,
-        default: new Date(),
+        default: () => {
+          const now = new Date();
+          const options = {
+            day: "numeric",
+            month: "numeric",
+            year: "numeric",
+          };
+
+          return new Intl.DateTimeFormat("en-GB", options).format(now);
+        },
+      },
+      complete: Boolean,
     },
-    userID: Number,
+  ],
+  frequency: {
+    daily: Boolean,
+    weekly: Boolean,
+  },
+  createdAt: {
+    type: String,
+    default: new Date(),
+  },
+  userID: Number,
 });
 
 habitSchema.statics.getUsersHabits = function (userID) {
-    return new Promise(async function (resolve, reject) {
-        try {
-            const user = await User.findByUsername(userID);
-            const habits = await this.find({ userID: user.id }).toArray();
-            resolve(habits);
-        } catch (error) {
-            reject("No habits found");
-        }
-    });
+  return new Promise(async function (resolve, reject) {
+    try {
+      //  const user = await User.findByUsername(userID);
+      const habits = await Habit.find({ userID: userID });
+      resolve(habits);
+    } catch (error) {
+      reject("No habits found");
+    }
+  });
 };
 
 habitSchema.statics.getSpecificHabit = function (userID, habitName) {
-    return new Promise(async function (resolve, reject) {
-        try {
-            const habit = await this.where("userID").equals(userID).where("name").equals(habitName);
-            resolve(habit);
-        } catch (error) {
-            reject("No habit found");
-        }
-    });
+  return new Promise(async function (resolve, reject) {
+    try {
+      const habit = await this.where("userID")
+        .equals(userID)
+        .where("name")
+        .equals(habitName);
+      resolve(habit);
+    } catch (error) {
+      reject("No habit found");
+    }
+  });
 };
 
 habitSchema.statics.destroy = function (habitData) {
-    return new Promise(async function (resolve, reject) {
-        try {
-            const habit = await this.deleteOne({ ...habitData });
-            resolve(habit);
-        } catch (error) {
-            reject("No habit found");
-        }
-    });
+  return new Promise(async function (resolve, reject) {
+    try {
+      const habit = await this.deleteOne({ ...habitData });
+      resolve(habit);
+    } catch (error) {
+      reject("No habit found");
+    }
+  });
 };
 
 habitSchema.statics.findByName = function (name) {
-    return new Promise(async function (resolve, reject) {
-        try {
-            const habit = await this.findOne({ name: name });
-            resolve(habit);
-        } catch (error) {
-            reject("Habit not found");
-        }
-    });
+  return new Promise(async function (resolve, reject) {
+    try {
+      const habit = await this.findOne({ name: name });
+      resolve(habit);
+    } catch (error) {
+      reject("Habit not found");
+    }
+  });
 };
 
 const Habit = mongoose.model("Habit", habitSchema);
--- client/static/js/layout.js
+++ client/static/js/layout.js
@@ -1,3 +1,8 @@
+const myDate = new Date();
+//const nextDayOfMonth = myDate.getDate() + 20;
+//myDate.setDate(nextDayOfMonth);
+//const newDate = myDate.toLocaleString();
+console.log(myDate);
 // Placeholder id's have been used, will be changed to reflect the actual HTML element ID's
 const createMenuBtn = document.getElementById("createMenuBtn");
 const createMenu = document.getElementById("createMenu");
@@ -22,94 +27,81 @@ const habitCollection = document.getElementById("greetingPost"); // will change 
 
 function displayAllHabits() {
   const habits = getAllHabits();
-  const { name, tracking, frequency, streak } = habits.habits; //not sure what datapoints will be fetched from DB yet
+  for (let i = 0; i < habits.length; i++) {
+    const { name, frequency } = habits[i];
+    let id = i;
+    const habitContainer = document.createElement("div");
+    habitContainer.classList = "habit-container";
 
-  const habitContainer = document.createElement("div");
-  habitContainer.classList = "habit-container";
+    const habitHeader = document.createElement("div");
+    habitHeader.classList = "habit-header";
+    const habitBody = document.createElement("div");
+    habitBody.classList = "habit-body";
+    const habitFooter = document.createElement("div");
+    habitFooter.classList = "habit-footer";
 
-  const habitHeader = document.createElement("div");
-  habitHeader.classList = "habit-header";
-  const habitBody = document.createElement("div");
-  habitBody.classList = "habit-body";
-  const habitFooter = document.createElement("div");
-  habitFooter.classList = "habit-footer";
+    const habitName = document.createElement("p");
+    habitName.classList = "habit-name";
+    habitName.textContent = name;
+    habitHeader.appendChild(habitName);
 
-  const habitName = document.createElement("p");
-  habitName.classList = "habit-name";
-  habitName.textContent = "Row my boat gently down the stream";
-  habitHeader.appendChild(habitName);
+    const habitInfo = document.createElement("p");
+    const frequencyInfo = document.createElement("p");
+    frequencyInfo.classList = "habit-information";
+    habitInfo.classList = "habit-information";
+    habitInfo.textContent = "tracking info";
+    frequencyInfo.textContent = frequency;
+    habitBody.appendChild(habitInfo);
+    habitBody.appendChild(frequencyInfo);
 
-  const habitInfo = document.createElement("p");
-  const frequencyInfo = document.createElement("p");
-  frequencyInfo.classList = "habit-information";
-  habitInfo.classList = "habit-information";
-  habitInfo.textContent = "tracking info";
-  frequencyInfo.textContent = `This should be completed weekly`;
-  habitBody.appendChild(habitInfo);
-  habitBody.appendChild(frequencyInfo);
+    const streakInfo = document.createElement("p");
+    streakInfo.classList = "streak-information";
+    streakInfo.textContent = "3";
+    habitFooter.appendChild(streakInfo);
+    // if (frequency.daily === true) {
 
-  const streakInfo = document.createElement("p");
-  streakInfo.classList = "streak-information";
-  streakInfo.textContent = "3";
-  habitFooter.appendChild(streakInfo);
-  // if (dailyOrWeekly === "daily") {
-  // id of the habit will be used in place of 1 when the data is fetched successfully.
-  const mon = document.createElement("button");
-  mon.classList = "habit-complete-button";
-  mon.id = `1`;
+    const mon = document.createElement("button");
+    mon.classList = "habit-complete-button";
+    mon.id = `${id}`;
 
-  const tue = document.createElement("button");
-  tue.classList = "habit-complete-button";
-  tue.id = `1`;
+    const tue = document.createElement("button");
+    tue.classList = "habit-complete-button";
+    tue.id = `${id}`;
 
-  const wed = document.createElement("button");
-  wed.classList = "habit-complete-button";
-  wed.id = `1`;
+    const wed = document.createElement("button");
+    wed.classList = "habit-complete-button";
+    wed.id = `${id}`;
 
-  const thu = document.createElement("button");
-  thu.classList = "habit-complete-button";
-  thu.id = `1`;
+    const thu = document.createElement("button");
+    thu.classList = "habit-complete-button";
+    thu.id = `${id}`;
 
-  const fri = document.createElement("button");
-  fri.classList = "habit-complete-button";
-  fri.id = `1`;
+    const fri = document.createElement("button");
+    fri.classList = "habit-complete-button";
+    fri.id = `${id}`;
 
-  const sat = document.createElement("button");
-  sat.classList = "habit-complete-button";
-  sat.id = `1`;
+    const sat = document.createElement("button");
+    sat.classList = "habit-complete-button";
+    sat.id = `${id}`;
 
-  const sun = document.createElement("button");
-  sun.classList = "habit-complete-button";
-  sun.id = `1`;
+    const sun = document.createElement("button");
+    sun.classList = "habit-complete-button";
+    sun.id = `${id}`;
 
-  habitFooter.appendChild(mon);
-  habitFooter.appendChild(tue);
-  habitFooter.appendChild(wed);
-  habitFooter.appendChild(thu);
-  habitFooter.appendChild(fri);
-  habitFooter.appendChild(sat);
-  habitFooter.appendChild(sun);
-  // }
-  habitContainer.appendChild(habitHeader);
-  habitContainer.appendChild(habitBody);
-  habitContainer.appendChild(habitFooter);
+    habitFooter.appendChild(mon);
+    habitFooter.appendChild(tue);
+    habitFooter.appendChild(wed);
+    habitFooter.appendChild(thu);
+    habitFooter.appendChild(fri);
+    habitFooter.appendChild(sat);
+    habitFooter.appendChild(sun);
+    // }
+    habitContainer.appendChild(habitHeader);
+    habitContainer.appendChild(habitBody);
+    habitContainer.appendChild(habitFooter);
 
-  habitCollection.appendChild(habitContainer);
+    habitCollection.appendChild(habitContainer);
+  }
 }
 
 displayAllHabits();
-
-// Javascript for the tracking information buttons
-
-const trackerButtons = document.querySelectorAll();
-
-let currentDate = new Date();
-
-Array.from(trackerButtons).forEach((b) => {
-  b.addEventListener("click", () => {
-    data = getTrackingInfo(b.id);
-    let currentDate = new Date();
-    if (currentDate != data.trackingSchema.dates.date) {
-    }
-  });
-});
