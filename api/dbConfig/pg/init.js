13b52d512a0cc14e8d0a70f8e6df2ef34dea05c5
Peesh98 <timothy.t.le98@gmail.com>
2022-07-28 19:21:32 +0100

    Edited pg database connection

--- api/dbConfig/pg/init.js
+++ api/dbConfig/pg/init.js
@@ -1,5 +1,20 @@
-const { Pool } = require("pg");
+const { Client } = require('pg');
 
-const pool = new Pool();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-module.exports = pool;
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
+
+module.exports = client;
